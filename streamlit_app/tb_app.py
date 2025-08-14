d0effd8a89e5ce18cbd6c6376d45a0e6baf0ddde
Boughdiri Mohamed <mboughdiri5@gmail.com>
2025-08-14 17:45:23 +0100

    Update tb_app.py

--- streamlit_app/tb_app.py
+++ streamlit_app/tb_app.py
@@ -49,7 +49,8 @@ uploaded_file = st.file_uploader("Choose an X-ray image...", type=["jpg", "png",
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Uploaded Image', use_container_width=True)
+
     
     pred_class, confidence = predict(image)
     st.write(f"**Prediction:** {pred_class}")
